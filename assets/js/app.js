0960ab607ed721167c52c481b5d4b492d301aaa8
thewebarif <thewebarif@gmail.com>
2024-06-19 11:51:53 +0600

    index.html

---
+++ assets/js/app.js
@@ -0,0 +1,37 @@
+/*
+=====================
+JS Table of Conttent 
+=====================
+01. Mobile Menu 
+04. AOS Animation
+
+*/
+
+(function ($) {
+  "use strict";
+
+
+ /*
+  ------------------------  
+  01. Mobile Menu 
+  --------------------------
+  */
+  $(".mobile-toggle").on("click", function () {
+    $(this).toggleClass("open");
+    $(".toggle-menu-class").toggleClass("mobile-device-active");
+  });
+  $(".toggle").on("click", function () {
+    if ($(this).text().includes("-")) {
+      $(this).text("+")
+    } else {
+      $(this).text("-")
+    }
+    $(this).parent().siblings(".submenu").slideToggle();
+  });
+
+
+  
+}(jQuery));
+
+
+
